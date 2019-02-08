8e2dc3fc01b7d96f170f32791bfce61eb2980c6b
GeorgeIlincuta <GeorgeIlincuta@users.noreply.github.com>
2019-02-08 14:59:23 +0200

    Skeleton files

---
+++ src/components/StorePicker.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+
+    render() {
+        return (
+            <p>test</p>
+        )
+    }
+    
+}
+export default StorePicker;
