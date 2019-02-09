ff7764eaacf3a6621df831d7de428a2b53f74bdc
GeorgeIlincuta <GeorgeIlincuta@users.noreply.github.com>
2019-02-09 21:02:01 +0200

    First 3 components

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -4,7 +4,9 @@ class StorePicker extends React.Component {
 
     render() {
         return (
-            <p>test</p>
+            <form className="store-selector">
+                <h2>Please Enter A Store</h2>
+            </form>
         )
     }
     
